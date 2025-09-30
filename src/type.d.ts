fb623fc2dc20e9186c659e84511fd4f32234adf4
Swizzer <kyoshaft@outlook.com>
2025-09-30 18:42:21 +0800

    feat: partial encryption

--- src/type.d.ts
+++ src/type.d.ts
@@ -2,3 +2,8 @@ declare module 'virtual:config' {
   const Config: import('astro-pure/types').ConfigOutput
   export default Config
 }
+
+declare module 'crypto-js' {
+  const CryptoJS: any
+  export default CryptoJS
+}
